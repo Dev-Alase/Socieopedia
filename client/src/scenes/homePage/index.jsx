0c8343619ea942281f2e9a1f25902a86328a8a19
Dev-Alase <devalase5@gmail.com>
2024-05-31 22:57:32 +0530

    done ans dusted,but still there is some error need to handle

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,9 @@ import { useSelector } from "react-redux"
 import Navbar from "scenes/navbar"                       // eslint-disable-next-line
 import UserWidget from "scenes/widgets/UserWidget" 
 import MyPostWidget from "scenes/widgets/MyPostWidget"
+import PostsWidget from "scenes/widgets/PostsWidget"
+import AdvertWidget from "scenes/widgets/AdvertWidget"
+import FriendListWidget from "scenes/widgets/FriendListWidget"
 
 const HomePage = () => {
 
@@ -22,10 +25,16 @@ const HomePage = () => {
 
             <Box flexBasis={isNonMobileScreens ? "42%" : undefined}  mt={isNonMobileScreens ? undefined : "2rem"}>
                <MyPostWidget picturePath={picturePath} />
+               <PostsWidget userId={_id}/>
             </Box>
 
             {isNonMobileScreens && (
-                <Box flexBasis="26%"> </Box>
+                <Box flexBasis="26%"> 
+                    <AdvertWidget/>
+                    <Box m="2rem 0">
+                        <FriendListWidget userId={_id}/>
+                    </Box>
+                </Box>
             )}
         </Box>
     </Box>
